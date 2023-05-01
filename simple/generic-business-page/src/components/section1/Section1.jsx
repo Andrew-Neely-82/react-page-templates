cac2d48704110ca6159078355651bbc9e6ce5fb5
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-04-30 22:52:25 -0500

    updating styling

--- simple/generic-business-page/src/components/section1/Section1.jsx
+++ simple/generic-business-page/src/components/section1/Section1.jsx
@@ -5,7 +5,7 @@ const Section1 = () => {
   return (
     <div className="Section1__">
       <div className="Section1__content-container">
-        <h3>Lorem Ipsum</h3>
+        <h1>Lorem Ipsum</h1>
         <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </div>
